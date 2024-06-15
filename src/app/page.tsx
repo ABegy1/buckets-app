17b6b94a0993b4ec8bf005d47685ad3ee574de10
Aaron Begy <ab531217@ohio.edu>
2024-06-15 00:20:17 -0400

    changing auth timing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import db from "@/modules/db";
 import { revalidatePath } from "next/cache";
 import Button from "@/components/Button";
 import GoogleAuth from "@/components/GoogleAuth";
+import { useState } from "react";
 
 export default async function Home() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const posts = await db.post.findMany({ orderBy: { createdAt: "desc" } });
   const generatePosts = async () => {
     "use server";
@@ -18,15 +20,28 @@ export default async function Home() {
     });
     revalidatePath("/");
   };
+  const handleLoginSuccess = (response : any) => {
+    console.log('Login Success:', response);
+    setIsAuthenticated(true); // Update authentication state
+    // handle successful login
+  };
 
+  const handleLoginFailure = (response : any) => {
+    console.log('Login Failed:', response);
+    // handle failed login
+  };
   return (
-    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    <GoogleAuth />  
-    <Button onClick={generatePosts}>Generate Posts</Button>
-    {posts.map((post: any) => (
-      <div key={post.id}>{post.content}</div>
-    ))}
-  </main>
+      {!isAuthenticated ? (
+        <GoogleAuth onSuccess={handleLoginSuccess} onFailure={handleLoginFailure} />
+      ) : (
+        <>
+          <Button onClick={generatePosts}>Generate Posts</Button>
+          {posts.map((post: any) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </>
+      )}
+    </main>
   );
 }

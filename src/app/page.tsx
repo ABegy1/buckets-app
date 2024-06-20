799703f3119bb9938a55d7dfd5e018f44f82fb93
Aaron Begy <ab531217@ohio.edu>
2024-06-20 18:11:18 -0400

    update domain

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import { loginWithGoogle } from '../supabaseclient';
+import { loginWithGoogle } from '../supabaseClient';
 
 const Page = () => {
   const handleLogin = async () => {

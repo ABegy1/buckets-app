df509e9bdf5d61894c0311c09a1618680daddcbc
Aaron Begy <ab531217@ohio.edu>
2024-06-20 16:45:02 -0400

    connecting drizzle to supabase

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
 import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 
-config({ path: '.env.local' });
+config({ path: '.env' });
 
 export default defineConfig({
   schema: './src/db/schema.ts',
-  out: './migrations',
+  out: './supabase/migrations',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.POSTGRES_URL!,
+    url: process.env.DATABASE_URL!,
   },
 });

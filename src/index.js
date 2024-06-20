b326a009c107c8dfb09864461e8a47b0460120d6
Aaron Begy <ab531217@ohio.edu>
2024-06-20 17:47:08 -0400

    update 5

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import './index.css'
+import { useState, useEffect } from 'react'
+import { createClient } from '@supabase/supabase-js'
+import { Auth } from '@supabase/auth-ui-react'
+import { ThemeSupa } from '@supabase/auth-ui-shared'
+
+const supabase = createClient('https://pigzvnjbzrlitabxqtsm.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpZ3p2bmpienJsaXRhYnhxdHNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5MTQ4NDksImV4cCI6MjAzNDQ5MDg0OX0.RjkRYiCcRQLYto2ensG1wZCXmQFxjR-QhyRR1qZV0iA')
+
+export default function App() {
+  const [session, setSession] = useState(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
+  }
+  else {
+    return (<div>Logged in!</div>)
+  }
+}

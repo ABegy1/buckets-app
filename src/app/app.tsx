ebf20d44e2ea3cd5ce21db25e85c76aa231a08e8
Aaron Begy <ab531217@ohio.edu>
2024-06-20 17:35:08 -0400

    adding client tags

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import { AuthProvider } from '../components/useAuth'; // Adjust the path as needed
 import Page from './page'; // Adjust the path as needed

691b544072042fdb01ff1afd4509e6335e5f4fb0
Aaron Begy <ab531217@ohio.edu>
2024-06-20 23:12:52 -0400

    updating module import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { supabase } from '../supabaseClient';
 import type { User } from '@supabase/supabase-js';
 import AddUser from '@/components/AddDummyUser';
-import '../styles/globals.css';
+import './global.css';
 import Link from 'next/link';
 
 const useUserRole = (fullName: string) => {

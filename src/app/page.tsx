18a4acffbb4ccbad9c460b6aff6f7dd0538ceaeb
Aaron Begy <ab531217@ohio.edu>
2024-07-29 22:08:44 -0400

    updating console.log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,6 +33,7 @@ const useUserRole = (fullName: string) => {
 
 const HomePage = () => {
   const [user, setUser] = useState<User | null>(null);
+  console.log(user);
   const { role, loading  } = useUserRole(user?.user_metadata.full_name ?? '');
 
   useEffect(() => {

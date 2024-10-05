67d7167e77225468c155a06f34baf7c5720c06fc
Abegy1 <abegy7@gmail.com>
2024-10-05 15:31:30 -0400

    adding signout buttons

--- src/app/Admin/page.tsx
+++ src/app/Admin/page.tsx
@@ -1,6 +1,7 @@
 'use client'; // Required in Next.js App Router
 import React from 'react';
 import styles from './adminPage.module.css'; // Import the CSS module
+import { supabase } from '@/supabaseClient'; // Import the Supabase client
 
 const AdminPage = () => {
   return (
@@ -10,6 +11,7 @@ const AdminPage = () => {
       </header>
       <main className={styles.adminContent}>
         <p>Welcome, Admin! You have successfully been redirected to the admin dashboard.</p>
+        <button className="btn" onClick={async () => await supabase.auth.signOut()}>Sign Out</button>
       </main>
       <footer className={styles.adminFooter}>
         <p>&copy; 2024 Buckets Game. Admin Panel. All rights reserved.</p>
--- src/app/User/page.tsx
+++ src/app/User/page.tsx
@@ -1,6 +1,7 @@
 'use client'; // Required in Next.js App Router
 import React from 'react';
 import styles from './UserPage.module.css'; // Import the CSS module
+import { supabase } from '@/supabaseClient';
 
 const UserPage = () => {
   return (
@@ -10,6 +11,7 @@ const UserPage = () => {
       </header>
       <main className={styles.userContent}>
         <p>Welcome, User! You have successfully been redirected to the user dashboard.</p>
+        <button className="btn" onClick={async () => await supabase.auth.signOut()}>Sign Out</button>
       </main>
       <footer className={styles.userFooter}>
         <p>&copy; 2024 Buckets Game. user Panel. All rights reserved.</p>

bdeef62929b7cbbd2bacbb4ee3294c0d04541ff0
jb664206 <104904268+jb664206@users.noreply.github.com>
2026-01-04 18:18:41 -0500

    Remove shots left dashes from database usage

--- src/components/NextSeason/NextSeason.tsx
+++ src/components/NextSeason/NextSeason.tsx
@@ -798,7 +798,6 @@ const NextSeasonModal: React.FC<NextSeasonModalProps> = ({ isOpen, onClose, onSt
           player_id: player.player_id,
           season_id: newSeasonId,
           shots_left: shotCount,
-          shots_left_dashes: 0,
           score: 0,
         }));
 
